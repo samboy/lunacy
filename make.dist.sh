d8d8195c8699cf6afe89209c0333d022648424a2
Sam Trenholme <maradns@gmail.com>
2020-06-30 13:53:53 -0500

    make.dist.sh: Finish up

--- make.dist.sh
+++ make.dist.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+rm -fr lunacy-dist/
 mkdir lunacy-dist/
 cp bin/lunacy.exe lunacy-dist/
 cp doc/lunacy-manual.html lunacy-dist/
+mkdir lunacy-dist/examples/
+cp test/*lua lunacy-dist/examples
+mkdir lunacy-dist/LUAstuff
+cp LUAstuff/* lunacy-dist/LUAstuff/
+cp COPYING lunacy-dist/COPYING.txt
+cp README.md lunacy-dist/README.txt
+unix2dos lunacy-dist/COPYING.txt
+unix2dos lunacy-dist/README.txt
+mv lunacy-dist/LUAstuff/COPYING.md lunacy-dist/LUAstuff/COPYING.txt
+unix2dos lunacy-dist/LUAstuff/COPYING.txt
