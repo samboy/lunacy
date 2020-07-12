14003b1269fdd2a98d18a0f5b2da5d70366e249d
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-12 15:07:06 -0700

    Add support for Lua 5.2 bit32 library

--- src/lualib.h
+++ src/lualib.h
@@ -42,6 +42,8 @@ LUALIB_API int (luaopen_package) (lua_State *L);
 #define LUA_LFSLIBNAME "lfs"
 LUALIB_API int (luaopen_lfs) (lua_State * L);
 
+#define LUA_BITLIBNAME  "bit32"
+LUAMOD_API int (luaopen_bit32) (lua_State *L);
 
 /* open all previous libraries */
 LUALIB_API void (luaL_openlibs) (lua_State *L); 
