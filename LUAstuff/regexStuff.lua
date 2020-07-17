594f7d18ae5b0d1677dbd08bd415d00021bc14b3
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-16 20:54:11 -0700

    regexStuff: Better CSV processing, human readable numbers, etc.

--- LUAstuff/regexStuff.lua
+++ LUAstuff/regexStuff.lua
@@ -3,6 +3,11 @@
 -- Output: An array (numeric indexed table) with the split string
 -- Should the character we split on not be in the string to split, 
 -- we will have a one-element array with the input string
+--
+-- Note: Should the splitter regex be a single character, things will 
+-- split as expected, but 0-length strings are converted in to a 
+-- one-character long space (" ") string.
+--
 -- Example usage:
 -- a = "1,2,3" t = rSplit(a,",") for k,v in pairs(t) do print(k,v) end
 function rCharSplit(i, c)
@@ -22,3 +27,88 @@ function rCharSplit(i, c)
   end
   return out
 end 
+
+----------------------- rStrSplit() -----------------------
+-- This does a simple split for a given string, useful for simple CSV
+-- Input: string (single CSV line), split character 
+-- Output: An array with each field in the CSV line
+function rStrSplit(s, splitOn)
+  if not splitOn then splitOn = "," end
+  local place = 1
+  local out = {} 
+  local last = 1
+  while place do
+    place = string.find(s, splitOn, place, true) 
+    if place then
+      table.insert(out,string.sub(s, last, place - 1))
+      place = place + 1
+      last = place
+    end
+  end 
+  table.insert(out,string.sub(s, last, -1))
+  return out
+end
+
+----------------------- qCsvSplit() -----------------------
+-- This handles the pesky issue of quoted CSV like this:
+-- '"foo, bar",2,"field 3, you know",This is field 4'
+-- s: String to split (a single CSV line)
+-- splitOn: Character to split on
+-- leftQuote: The character which starts a quote
+-- rightQuote: The character which ends a quote
+-- Output: An array with each field in the CSV line
+-- Note that leftQuote and rightQuote do not included in the
+-- final output array, since the usual intention with quoted CSV is to
+-- not have the quotes in the actual fields
+function qCsvSplit(s, splitOn, leftQuote, rightQuote)
+  if not splitOn then splitOn = "," end
+  if not leftQuote then leftQuote = '"' end
+  if not rightQuote then rightQuote = leftQuote end
+  local out = {}
+  local place = 1
+  local lastSplitter = 1
+  local state = 1 -- Finite state machine state
+  local toAdd = ""
+  while place <= string.len(s) do
+    local look = string.sub(s,place,place)
+    if look == splitOn and state == 1 then
+      table.insert(out,toAdd) 
+      toAdd = ""
+    elseif look == leftQuote and state == 1 then
+      state = 2
+    elseif look == rightQuote and state == 2 then
+      state = 1
+    else
+      toAdd = toAdd .. look
+    end
+    place = place + 1
+  end
+  table.insert(out,toAdd)
+  return out
+end
+
+-- Make a large number look like a nice number with commas
+-- Input: The number, separator (make "." for European numbers)
+-- Output: A string with the number looking nice
+-- Example usage:
+-- foo = 1234567890 print(humanNumber(foo,",","%d"))
+function humanNumber(n, separator, fmt)
+  if not fmt then fmt = "%.2f" end
+  if not separator then separator = "," end -- I am in the US
+  if n < 0 then return "ERROR" end
+  local out = ""
+  local parts = {}
+  if n < 1000 then return string.format(fmt, n) end
+  low = n % 1000
+  n = math.floor(n / 1000)
+  while n > 0 do
+    if n > 0 then table.insert(parts, n % 1000) end
+    n = math.floor(n / 1000)
+  end
+  for i = #parts, 1, -1 do
+    if i == #parts then out = tostring(parts[i])
+    else out = out .. separator .. string.format("%03d",parts[i]) end
+  end
+  out = out .. separator .. string.format(fmt, low)
+  return out
+end
--- LUAstuff/regexStuffTest.lua
+++ LUAstuff/regexStuffTest.lua
@@ -7,3 +7,30 @@ print("")
 
 b="This is a big  test!"
 tablePrint(rCharSplit(b,"%s"))
+
+--- rStrSplit
+b="1,,2,3,45,Hello there!,6,789"
+print("rStrSplit test #1 ---------------------------")
+tablePrint(rStrSplit(a))
+print("rStrSplit test #2 ---------------------------")
+tablePrint(rStrSplit(b))
+print("rStrSplit test #3 ---------------------------")
+for i,v in ipairs(rStrSplit(a)) do
+  print(v,string.len(v))
+end
+-- In comparison, this will not have a 0 len string
+print("rStrSplit test #4 ---------------------------")
+for i,v in ipairs(rCharSplit(a,",")) do
+  print(v,string.len(v))
+end
+c='"foo, bar",2,"field 3, you know",This is field 4'
+-- This is where rStrSplit breaks down
+print("rStrSplit test #5 ---------------------------")
+for i,v in ipairs(rStrSplit(c)) do
+  print(v,string.len(v))
+end
+-- So we need something which can handle quoted CSV stuff
+print("qCsvSplit test ------------------------------")
+for i,v in ipairs(qCsvSplit(c)) do
+  print(v,string.len(v))
+end
