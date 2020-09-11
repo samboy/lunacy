3efcabcff311a0d1b795ec791de734e806b4a032
Sam Trenholme <git@samiam.org>
2020-09-10 19:46:54 -0700

    Remove mmLunacyDNS: Use coLunacyDNS w/ MaraDNS instead
    Look in deadwood-github/tools/coLunacyDNS of any recent
    checkout (or release, 3.5.0012 and higher) of MaraDNS

--- mmLunacyDNS/compile.mmLunacyDNS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# With only one file, I will not bother with a Makefile
-
-cd ..
-make 
-cd mmLunacyDNS
-gcc -Os -c -o mmLunacyDNS.o mmLunacyDNS.c
-gcc -Os -o mmLunacyDNS mmLunacyDNS.o ../src/liblua.a -lm
