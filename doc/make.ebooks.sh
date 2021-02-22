64573b6f4ae2a23d9f67f4f213e43f274854dc97
Sam Trenholme <gravatar@samiam.org>
2021-02-22 01:20:52 -0800

    make.ebooks.sh: Also make the text manual

--- doc/make.ebooks.sh
+++ doc/make.ebooks.sh
@@ -2,3 +2,4 @@
 ebook-convert lunacy-manual.html lunacy-manual.mobi \
 	--mobi-file-type=both --change-justification=left 
 ebook-convert lunacy-manual.html lunacy-manual.epub
+lynx -dump -nolist lunacy-manual.html > lunacy-manual.txt
