419e63c49211a3d11b379755b08a0fcbbd366824
Sam Trenholme <maradns@samiam.org>
2021-04-03 21:39:45 -0700

    make.dist: OK, we really should have the spawner copyright there
    No, I do not think Steve Donovan cares either way.  However, there are
    random anonymous identities which take software licenses very seriously
    (Donovan isn’t one of them).

--- make.dist.sh
+++ make.dist.sh
@@ -13,8 +13,10 @@ rm lunacy-dist/LUAstuff/rg32*
 rm lunacy-dist/LUAstuff/bit32.lua
 rm lunacy-dist/LUAstuff/Lua51calc.lua
 cp COPYING lunacy-dist/COPYING.txt
+cp COPYING.spawner lunacy-dist/COPYING.spawner.txt
 cp README.md lunacy-dist/README.txt
 unix2dos lunacy-dist/COPYING.txt
+unix2dos lunacy-dist/COPYING.spawner.txt
 unix2dos lunacy-dist/README.txt
 mv lunacy-dist/LUAstuff/COPYING.md lunacy-dist/LUAstuff/COPYING.txt
 unix2dos lunacy-dist/LUAstuff/COPYING.txt
