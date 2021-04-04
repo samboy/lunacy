9d97e7f9e9088286276d22a9df1aac7dcb115e05
Sam Trenholme <maradns@samiam.org>
2021-04-03 23:40:21 -0700

    object2: Better comments

--- LUAstuff/object2.lua
+++ LUAstuff/object2.lua
@@ -2,9 +2,11 @@
 --
 -- This version uses metatables instead of object function factories
 
-Add1Obj = {}
-Add1Obj.__index = Add1Obj
+-- Let’s create a class
+Add1Obj = {} -- Initialize a new class
+Add1Obj.__index = Add1Obj -- Allow class to be a metatable
 
+-- Method: Initialize class
 function Add1Obj:new(start) 
   if start == nil then
     start = 1
@@ -12,24 +14,29 @@ function Add1Obj:new(start)
   return setmetatable({v = start},self)
 end
 
+-- Method: Add one to the class’s “v” element
 function Add1Obj:add1()
   self.v = self.v + 1
   return self.v
 end
 
+-- Example of class usage
 foo = Add1Obj:new(1)
 print(foo:add1())
 print(foo:add1())
 
-Add2Obj = {}
-Add2Obj.__index = Add2Obj
-setmetatable(Add2Obj, Add1Obj)
+-- Now, let’s create a derived class
+Add2Obj = {} -- Initialize a new class
+Add2Obj.__index = Add2Obj -- Allow this class to be a metatable
+setmetatable(Add2Obj, Add1Obj) -- Inherit the Add1Obj class
 
+-- Derived class has a new method
 function Add2Obj:add2()
   self.v = self.v + 2
   return self.v
 end
 
+-- Example using derived class
 foo = Add2Obj:new(20)
 print(foo:add1())
 print(foo:add2())
