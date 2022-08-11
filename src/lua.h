71a7629c0a72b47ea26a003327bdfd9db18c75a7
Sam Trenholme <maradns@samiam.org>
2022-08-11 11:24:35 -0700

    lua.h: Update version number
    2022-08-11 fixes bit32.rrotate to not trigger undefined behavior

--- src/lua.h
+++ src/lua.h
@@ -18,9 +18,9 @@
 
 
 #define LUA_VERSION     "Lunacy 5.1"
-#define LUA_RELEASE     "Lunacy 2021-07-28"
+#define LUA_RELEASE     "Lunacy 2022-08-11"
 #define LUA_VERSION_NUM 501
-#define LUA_COPYRIGHT   "Copyright 1994-2012 Lua.org PUC-Rio; 2020-2021 Sam Trenholme"
+#define LUA_COPYRIGHT   "Copyright 1994-2012 Lua.org PUC-Rio; 2020-2022 Sam Trenholme"
 #define LUA_AUTHORS     "R. Ierusalimschy, L. H. de Figueiredo, W. Celes, Sam Trenholme"
 
 
