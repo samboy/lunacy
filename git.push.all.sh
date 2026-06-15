2fa33d84cc49982a8228d02b1236dd0deb4ecf0a
Sam Trenholme <git@samiam.org>
2026-06-15 14:24:24 -0700

    Add git.push.all.sh to lunacy repo

---
+++ git.push.all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+for a in origin sourcehut codeberg ; do
+  echo $a
+  git push $a
+  git push $a --tags 
+  echo
+done
+
+echo Gitlab annoyance: I had to use Chrome to update my SSH key
+git push gitlab
+echo
+
+cat > /dev/null << EOF
+[remote "origin"]
+        url = git@github.com:samboy/lunacy.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[remote "sourcehut"]
+        url = git@git.sr.ht:~samiam/Lunacy
+[remote "codeberg"]
+        url = git@codeberg.org:samboy/lunacy.git
+EOF
+
